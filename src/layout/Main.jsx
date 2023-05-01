eda59f3ef6a10f73dc98d4534d349598155e313a
NazmulHasan18 <nazmul182218@gmail.com>
2023-05-01 11:03:16 +0600

    bootstrap react project created

---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+   return (
+      <div>
+         <h2>This is main layout</h2>
+         <p>How are you</p>
+         <button>Hi</button>
+      </div>
+   );
+};
+
+export default Main;
